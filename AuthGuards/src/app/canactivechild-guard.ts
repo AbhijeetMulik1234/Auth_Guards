716727df4813db62cfdcdaaa8fc7663a45478f65
AbhijeetMulik1234 <mulikabhijeet948@gmail.com>
2025-08-15 10:46:29 +0530

    code check in
    angular demo

--- AuthGuards/src/app/canactivechild-guard.ts
+++ AuthGuards/src/app/canactivechild-guard.ts
@@ -2,10 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateChildFn, Router } from '@angular/router';
 
 export const canactivechildGuard: CanActivateChildFn = (childRoute, state) => {
+  debugger;
   const router = inject(Router);
   const isLogedIn = localStorage.getItem('isLoggedItem');
 
   if (isLogedIn === 'true') {
+    debugger;
     return true;
   } else {
     router.navigate(['/login']);
